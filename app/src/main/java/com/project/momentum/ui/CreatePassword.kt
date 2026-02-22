19cacd62a27a6b636b090a9891f1a06595879805
guse95 <saveliy.gusev.05@list.ru>
2026-02-22 19:31:58 +0300

    deleted unnecessary imports

--- app/src/main/java/com/project/momentum/ui/CreatePassword.kt
+++ app/src/main/java/com/project/momentum/ui/CreatePassword.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.momentum.ConstColours
 import com.project.momentum.ContinueButton
 import com.project.momentum.R
