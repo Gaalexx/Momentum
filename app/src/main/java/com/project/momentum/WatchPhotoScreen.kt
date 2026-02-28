f3b70359c05deb63b4721260cfe7f89cd47c0a46
Gaalexx <gaalex05@gmail.com>
2026-02-28 12:32:05 +0300

    last fix after merge

--- app/src/main/java/com/project/momentum/WatchPhotoScreen.kt
+++ app/src/main/java/com/project/momentum/WatchPhotoScreen.kt
@@ -35,7 +35,6 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
@@ -46,13 +45,13 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
+//import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.momentum.ConstColours
-import com.project.momentum.ui.theme.AppTextStyles
+//import com.project.momentum.ui.theme.AppTextStyles
 import com.project.momentum.BigCircleForMainScreenAction
 import com.project.momentum.FriendsPillButton
 import com.project.momentum.ProfileCircleButton
