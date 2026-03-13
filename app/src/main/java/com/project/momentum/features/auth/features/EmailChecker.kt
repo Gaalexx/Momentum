9f1b59a06ea71330f91fd0a1df6d90159b1d777b
guse95 <saveliy.gusev.05@list.ru>
2026-03-13 18:57:03 +0300

    EmailChecker with bad API

--- app/src/main/java/com/project/momentum/features/auth/features/EmailChecker.kt
+++ app/src/main/java/com/project/momentum/features/auth/features/EmailChecker.kt
@@ -1,27 +1,19 @@
 package com.project.momentum.features.auth.features
 
+import android.util.Log
+import com.project.momentum.features.auth.models.dto.EmailResponse
+import jakarta.inject.Inject
 import jakarta.inject.Singleton
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.net.InetAddress
-import java.net.UnknownHostException
 
 @Singleton
-class EmailChecker {
-    suspend fun canReceiveEmail(email: String): Boolean = withContext(Dispatchers.IO) {
-        try {
-            val domain = email.substringAfter("@")
-            domainExists(domain)
-        } catch (e: Exception) {
-            false
-        }
-    }
-
-    private fun domainExists(domain: String): Boolean {
-        return try {
-            InetAddress.getAllByName(domain).isNotEmpty()
-        } catch (e: UnknownHostException) {
-            false
-        }
+class EmailChecker @Inject constructor(
+    private val client: EmailCheckerAPI,
+) {
+    suspend fun checkEmail(email: String): Boolean {
+        val response = client.checkEmail(email)
+        Log.d("EmailChecker", response.toString())
+        return response.smtpCheck
     }
 }
