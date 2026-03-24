a81b1fde24822d9ad5b3a036dcd08b2f19e20dd2
guse95 <saveliy.gusev.05@list.ru>
2026-03-24 18:01:09 +0300

    loading

--- app/src/main/java/com/project/momentum/features/editingAccount/EditingAccountScreen.kt
+++ app/src/main/java/com/project/momentum/features/editingAccount/EditingAccountScreen.kt
@@ -15,9 +15,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Divider
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -43,6 +44,7 @@ import com.project.momentum.ui.assets.CancelButton
 import com.project.momentum.ui.assets.ContinueButton
 import com.project.momentum.ui.assets.TextFieldRegistration
 import com.project.momentum.ui.assets.TopBarTemplate
+import com.project.momentum.ui.common.LoadingOverlay
 import com.project.momentum.ui.theme.AppTextStyles
 import com.project.momentum.ui.theme.ConstColours
 
@@ -87,151 +89,159 @@ fun EditingAccountScreen(
     modifier: Modifier = Modifier
 ) {
 
-    val subHeadLine: @Composable (String) -> Unit = {
+    val headLine: @Composable (String) -> Unit = {
         Text (
             text = it,
-            style = AppTextStyles.SubHeadlines,
+            style = AppTextStyles.Headlines,
             color = ConstColours.MAIN_BRAND_BLUE,
         )
     }
     @Composable
     fun EditTextField(
+        title: String,
         value: String,
         onValueChange: (String) -> Unit,
         placeholder: String? = null,
+        isError: Boolean = false,
         keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(
             keyboardType = KeyboardType.Text,
             imeAction = ImeAction.Next
         )
     ) {
+        Text (
+            text = title,
+            style = AppTextStyles.SubHeadlines,
+            color = ConstColours.MAIN_BRAND_BLUE,
+            modifier = Modifier
+                .padding(bottom = dimensionResource(R.dimen.small_padding))
+        )
+
         TextFieldRegistration(
             value = value,
             onValueChange = onValueChange,
-            modifier = Modifier
-                .height(48.dp)
-                .shadow(
-                    elevation = 8.dp,
-                    shape = RoundedCornerShape(36.dp),
-                    spotColor = ConstColours.MAIN_BRAND_BLUE
-                ),
+            modifier = Modifier,
             placeholder = placeholder,
+            isError = isError,
             keyboardOptions = keyboardOptions,
         )
     }
 
-    TopBarTemplate (
-        label = R.string.label_editing,
-        onBackClick = onBackClick,
-        modifier = modifier
-    ) {
-        val iconTint = Color(0xFFEDEEF2) // TODO: fa fa fa what a faaa
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
+    if (uiInfoState is EditAccountState.Loading) {
+        LoadingOverlay()
+    } else
+        TopBarTemplate (
+            label = R.string.label_editing,
+            onBackClick = onBackClick,
+            modifier = modifier
         ) {
-            Box(
+            val iconTint = Color(0xFFEDEEF2) // TODO: fa fa fa what a faaa
+
+            Column(
                 modifier = Modifier
-                    .padding(dimensionResource(R.dimen.medium_large_padding))
-                    .fillMaxWidth(),
-                contentAlignment = Alignment.Center
+                    .fillMaxSize()
             ) {
                 Box(
                     modifier = Modifier
-                        .size(120.dp)
-                        .clip(CircleShape)
-                        .background(ConstColours.MAIN_BACK_GRAY)
-                        .border(2.dp, ConstColours.MAIN_BRAND_BLUE, CircleShape)
+                        .padding(dimensionResource(R.dimen.medium_large_padding))
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center
                 ) {
-                    if (uiInfoState.fields.profilePhotoURL == null) {
-//                        Image(
-//                            painter = painterResource(R.drawable.profile_image_small),
-//                            contentDescription = null,
-//                            modifier = Modifier
-//                                .height(80.dp)
-//                                .aspectRatio(1f)
-//                                .align(Alignment.Center),
-//                        )
-                        Icon(
-                            imageVector = Icons.Outlined.AccountCircle,
-                            contentDescription = stringResource(R.string.account_avatar),
-                            tint = iconTint.copy(alpha = 0.7f),
-                            modifier = Modifier
-                                .size(80.dp)
-                                .align(Alignment.Center)
-                        )
-                    } else {
-                        AsyncImage(
-                            model = uiInfoState.fields.profilePhotoURL,
-                            contentDescription = stringResource(R.string.account_avatar),
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(2.dp)
-                                .clip(CircleShape)
-                        )
+                    Box(
+                        modifier = Modifier
+                            .size(120.dp)
+                            .clip(CircleShape)
+                            .background(ConstColours.MAIN_BACK_GRAY)
+                            .border(2.dp, ConstColours.MAIN_BRAND_BLUE, CircleShape)
+                    ) {
+                        if (uiInfoState.fields.profilePhotoURL == null) {
+    //                        Image(
+    //                            painter = painterResource(R.drawable.profile_image_small),
+    //                            contentDescription = null,
+    //                            modifier = Modifier
+    //                                .height(80.dp)
+    //                                .aspectRatio(1f)
+    //                                .align(Alignment.Center),
+    //                        )
+                            Icon(
+                                imageVector = Icons.Outlined.AccountCircle,
+                                contentDescription = stringResource(R.string.account_avatar),
+                                tint = iconTint.copy(alpha = 0.7f),
+                                modifier = Modifier
+                                    .size(80.dp)
+                                    .align(Alignment.Center)
+                            )
+                        } else {
+                            AsyncImage(
+                                model = uiInfoState.fields.profilePhotoURL,
+                                contentDescription = stringResource(R.string.account_avatar),
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(2.dp)
+                                    .clip(CircleShape)
+                            )
+                        }
                     }
                 }
-            }
-            subHeadLine("Основное")
-            Divider(
-                thickness = dimensionResource(R.dimen.thickness_divider),
-                color = ConstColours.WHITE,
-                modifier = Modifier.padding(top = dimensionResource(R.dimen.medium_padding))
-            )
-            Column(
-                verticalArrangement = Arrangement.spacedBy(
-                    dimensionResource(R.dimen.small_padding)
-                ),
-                modifier = Modifier
-                    .padding(dimensionResource(R.dimen.medium_padding))
-            ) {
-                subHeadLine("Логин")
-                EditTextField(
-                    value = uiInfoState.fields.username ?: "",
-                    onValueChange = onLoginChange,
+                headLine("Основное")
+                HorizontalDivider(
+                    modifier = Modifier.padding(top = dimensionResource(R.dimen.medium_padding)),
+                    thickness = dimensionResource(R.dimen.thickness_divider),
+                    color = ConstColours.WHITE
                 )
-                subHeadLine("Почта")
-                EditTextField(
-                    value = uiInfoState.fields.email ?: "",
-                    onValueChange = onEmailChange,
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        keyboardType = KeyboardType.Email,
-                        imeAction = ImeAction.Next
-                    )
-                )
-                subHeadLine("Телефон")
-                EditTextField(
-                    value = uiInfoState.fields.phone ?: "",
-                    onValueChange = onPhoneChange,
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        keyboardType = KeyboardType.Phone,
-                        imeAction = ImeAction.Done
-                    )
-                )
-            }
-            Spacer(Modifier.weight(1f))
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(
-                    dimensionResource(R.dimen.medium_padding)
-                )
-            ) {
-                CancelButton(
-                    onClick = onBackClick,
+                Column(
                     modifier = Modifier
-                        .height(dimensionResource(R.dimen.sub_button_size))
-                        .weight(1f),
-                )
-                ContinueButton(
-                    text = "Сохранить",
-                    onClick = onContinueClick,
-                    modifier = Modifier
-                        .height(dimensionResource(R.dimen.sub_button_size))
-                        .weight(1f)
-                )
+                        .padding(dimensionResource(R.dimen.medium_padding))
+                ) {
+                    EditTextField(
+                        title = "Логин",
+                        value = uiInfoState.fields.username ?: "",
+                        onValueChange = onLoginChange,
+                        isError = uiInfoState.getErrorForUsername() != null,
+                    )
+                    EditTextField(
+                        title = "Почта",
+                        value = uiInfoState.fields.email ?: "",
+                        onValueChange = onEmailChange,
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Email,
+                            imeAction = ImeAction.Next
+                        ),
+                        isError = uiInfoState.getErrorForEmail() != null,
+                    )
+                    EditTextField(
+                        title = "Телефон",
+                        value = uiInfoState.fields.phone ?: "",
+                        onValueChange = onPhoneChange,
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Phone,
+                            imeAction = ImeAction.Done
+                        ),
+                        isError = uiInfoState.getErrorForPhone() != null,
+                    )
+                }
+                Spacer(Modifier.weight(1f))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(
+                        dimensionResource(R.dimen.medium_padding)
+                    )
+                ) {
+                    CancelButton(
+                        onClick = onBackClick,
+                        modifier = Modifier
+                            .height(dimensionResource(R.dimen.sub_button_size))
+                            .weight(1f),
+                    )
+                    ContinueButton(
+                        text = "Сохранить",
+                        onClick = onContinueClick,
+                        modifier = Modifier
+                            .height(dimensionResource(R.dimen.sub_button_size))
+                            .weight(1f)
+                    )
+                }
             }
         }
-    }
 }
 
 @Preview
