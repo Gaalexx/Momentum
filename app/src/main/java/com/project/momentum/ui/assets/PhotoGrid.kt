f7d65196fe11c65d3f3e3cc07f5ae8099d09ef25
Gaalexx <gaalex05@gmail.com>
2026-04-15 14:38:22 +0300

    gradient

--- app/src/main/java/com/project/momentum/ui/assets/PhotoGrid.kt
+++ app/src/main/java/com/project/momentum/ui/assets/PhotoGrid.kt
@@ -8,17 +8,24 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.SubcomposeAsyncImage
 import com.project.momentum.R
 import com.project.momentum.features.account.models.PostData
+import com.project.momentum.network.s3.MediaType
+import com.project.momentum.ui.custom.gradientpicker.GradientPicker
+import com.project.momentum.ui.custom.shapes.ScallopedShape
 import com.project.momentum.ui.theme.ConstColours
 
 @Composable
@@ -121,7 +128,14 @@ fun S3PhotoGrid(
 ) {
     val displayItems = buildList<S3GridItem> {
         if (showPlusButton) add(S3GridItem.PlusButton)
-        posts.forEach { post -> add(S3GridItem.Post(post)) }
+        posts.forEach { post ->
+            when (post.mediaType) {
+                MediaType.IMAGE -> add(S3GridItem.Post(post))
+                MediaType.AUDIO -> add(S3GridItem.AudioPost(post, GradientPicker.pick()))
+                MediaType.VIDEO -> add(S3GridItem.VideoPost(post))
+            }
+
+        }
     }
 
     LazyVerticalGrid(
@@ -142,15 +156,16 @@ fun S3PhotoGrid(
                 }
             }
         ) { index, item ->
-            Box(
-                modifier = Modifier
-                    .aspectRatio(1f)
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(ConstColours.MAIN_BACK_GRAY)
-            ) {
-                when (item) {
-                    is S3GridItem.Post -> {
-                        val post = item.post
+
+            when (item) {
+                is S3GridItem.Post -> {
+                    val post = item.post
+                    Box(
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(ConstColours.MAIN_BACK_GRAY)
+                    ) {
                         SubcomposeAsyncImage(
                             model = post.presignedURL,
                             contentDescription = stringResource(
@@ -161,7 +176,6 @@ fun S3PhotoGrid(
                             modifier = Modifier
                                 .fillMaxSize()
                                 .clickable {
-                                    // Вычисляем индекс в оригинальном списке posts
                                     val postIndex = if (showPlusButton) index - 1 else index
                                     onPostClick(postIndex)
                                 },
@@ -179,26 +193,69 @@ fun S3PhotoGrid(
                             }
                         )
                     }
+                }
 
-                    is S3GridItem.AudioPost -> {
-
-                    }
-                    is S3GridItem.VideoPost -> {
-
-                    }
-
-                    S3GridItem.PlusButton -> {
-                        Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            PlusButton(
-                                onClick = onAddPhotoClick,
-                                modifier = Modifier.size(48.dp),
-                                backgroundColor = ConstColours.MAIN_BRAND_BLUE,
-                                iconColor = ConstColours.WHITE
+                is S3GridItem.AudioPost -> {
+                    val post = item.post
+                    Box(
+                        modifier = modifier
+                            .fillMaxSize()
+                            .aspectRatio(1f)
+                            .clip(ScallopedShape())
+                            .background(
+                                brush = item.brush
                             )
-                        }
+                            .clickable {
+                                val postIndex = if (showPlusButton) index - 1 else index
+                                onPostClick(postIndex)
+                            },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            modifier = Modifier.fillMaxSize(0.5f),
+                            imageVector = Icons.Outlined.PlayArrow,
+                            contentDescription = stringResource(R.string.play),
+                            tint = ConstColours.WHITE
+                        )
+                    }
+                }
+
+                is S3GridItem.VideoPost -> {
+                    Box(
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(ConstColours.MAIN_BACK_GRAY)
+                            .clickable {
+                                val postIndex = if (showPlusButton) index - 1 else index
+                                onPostClick(postIndex)
+                            },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            modifier = Modifier.fillMaxSize(0.5f),
+                            imageVector = Icons.Outlined.PlayArrow,
+                            contentDescription = stringResource(R.string.play),
+                            tint = ConstColours.WHITE
+                        )
+                    }
+                }
+
+                S3GridItem.PlusButton -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .aspectRatio(1f)
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(ConstColours.MAIN_BACK_GRAY),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        PlusButton(
+                            onClick = onAddPhotoClick,
+                            modifier = Modifier.size(48.dp),
+                            backgroundColor = ConstColours.MAIN_BRAND_BLUE,
+                            iconColor = ConstColours.WHITE
+                        )
                     }
                 }
             }
@@ -206,9 +263,10 @@ fun S3PhotoGrid(
     }
 }
 
+
 sealed class S3GridItem {
     data class Post(val post: PostData) : S3GridItem()
     data class VideoPost(val post: PostData) : S3GridItem()
-    data class AudioPost(val post: PostData) : S3GridItem()
+    data class AudioPost(val post: PostData, val brush: Brush) : S3GridItem()
     data object PlusButton : S3GridItem()
 }
