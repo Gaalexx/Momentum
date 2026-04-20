f9aed2a6e627767e835e2842b160b59491606c00
guse95 <saveliy.gusev.05@list.ru>
2026-04-20 16:25:40 +0300

    CaptionBasicLabelPreview

--- app/src/main/java/com/project/momentum/ui/assets/Labels.kt
+++ app/src/main/java/com/project/momentum/ui/assets/Labels.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -38,3 +39,11 @@ fun CaptionBasicLabel(
     }
 }
 
+@Preview
+@Composable
+fun CaptionBasicLabelPreview() {
+    CaptionBasicLabel(
+        text = "",
+        placeholder = "Placeholder"
+    )
+}
